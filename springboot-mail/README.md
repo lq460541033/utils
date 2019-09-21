886cbf474ce0708062435b76a2b5fd532defc853
luoqiang <luoqiang@efunong.com>
2019-09-21 10:08:34 +0800

    util

---
+++ springboot-mail/README.md
@@ -0,0 +1,28 @@
+# springboot2.x发送邮件
+
+#### 项目介绍
+使用springboot2.x来发送邮件的demo
+
+https://www.jianshu.com/p/59d15b357201
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 使用说明
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 参与贡献
+
+1. Fork 本项目
+2. 新建 Feat_xxx 分支
+3. 提交代码
+4. 新建 Pull Request
---
+++ springboot-mail/src/test/java/com/bpf/springbootmail/SpringbootMailApplicationTests.java
@@ -0,0 +1,59 @@
+package com.bpf.springbootmail;
+
+import com.bpf.springbootmail.service.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMailApplicationTests {
+
+	@Autowired
+	private MailService mailService;
+	@Autowired
+	private TemplateEngine templateEngine;
+
+	/**
+	 * 发送简单纯文本邮件
+	 */
+	@Test
+	public void sendSimpleMail() {
+		mailService.sendSimpleMail("luoqiang@efunong.com", "发送邮件测试", "大家好，这是我用springboot进行发送邮件测试");
+	}
+
+	/**
+	 * 发送HTML邮件
+	 */
+	@Test
+	public void sendHtmlMail() {
+		String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件" + "</font></h3></body></html>";
+		mailService.sendHtmlMail("luoqiang@efunong.com", "发送邮件测试", content);
+	}
+
+	/**
+	 * 发送带附件的邮件
+	 */
+	@Test
+	public void sendAttachmentMail() {
+		String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件，有附件哦" + "</font></h3></body></html>";
+		String filePath = "D:/java/new2.xls";
+		mailService.sendAttachmentMail("luoqiang@efunong.com", "发送邮件测试", content, filePath);
+	}
+
+	/**
+	 * 发送带图片的邮件
+	 */
+	@Test
+	public void sendInlineResourceMail() {
+		String rscPath = "your picture path";
+		String rscId = "001";
+		String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件，有图片哦" + "</font></h3>"
+				+ "<img src=\'cid:" + rscId + "\'></body></html>";
+		mailService.sendInlineResourceMail("receiver@email.com", "发送邮件测试", content, rscPath, rscId);
+	}
+
+}
